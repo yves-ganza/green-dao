9522dbc270ee735fee16986087712fdd95c30c10
lebonYG <lebonganza@outlook.com>
2021-12-20 12:27:47 -0500

    Initial commit: Added connect wallet button

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks'
+import 'regenerator-runtime/runtime'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const chains = [1, 4]
+  const connectors = {
+    injected: {},
+    walletconnect: {}
+  }
+
+  return(
+      <ThirdwebWeb3Provider connectors={connectors} supportedChainIds={chains}>
+        <Component {...pageProps} />
+      </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
